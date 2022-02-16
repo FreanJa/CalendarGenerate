9773b5639a4cbaa7349cd8c734baa112a160e3b9
FreanJa <freanja2730@outlook.com>
2022-02-16 22:38:57 +0800

    json format success

--- main.py
+++ main.py
@@ -1,21 +1,20 @@
 import copy
 import datetime
-
 import pdfplumber
 import json
 import re
 
-import sys
-
 Week = {"星期一": "Mon", "星期二": "Tue", "星期三": "Wed", "星期四": "Thur", "星期五": "Fri", "星期六": "Sat", "星期日": "Sun"}
-Time_N = {1: ["081000", "085500"], 2: ["090000", "094500"], 3: ["100000", "104500"], 4: ["104500", "113000"],
-          5: ["113500", "122000"], 6: ["133000", "141500"], 7: ["142000", "150500"], 8: ["151500", "160000"],
-          9: ["160500", "165000"], 10: ["183000", "191500"], 11: ["192000", "200500"], 12: ["201000", "205500"]}
-Time_S = {1: ["082000", "090500"], 2: ["090500", "094500"], 3: ["101500", "110000"], 4: ["110500", "115000"],
-          5: ["115000", "123500"], 6: ["133000", "141500"], 7: ["142000", "150500"], 8: ["151500", "160000"],
-          9: ["160500", "165000"], 10: ["183000", "191500"], 11: ["192000", "200500"], 12: ["201000", "205500"]}
+Offset = {"Mon": 0, "Tue": 1, "Wed": 2, "Thur": 3, "Fri": 4, "Sat": 5, "Sun": 6}
+Time_N = {"1": ["081000", "085500"], "2": ["090000", "094500"], "3": ["100000", "104500"], "4": ["104500", "113000"],
+          "5": ["113500", "122000"], "6": ["133000", "141500"], "7": ["142000", "150500"], "8": ["151500", "160000"],
+          "9": ["160500", "165000"], "10": ["183000", "191500"], "11": ["192000", "200500"], "12": ["201000", "205500"]}
+Time_S = {"1": ["082000", "090500"], "2": ["090500", "094500"], "3": ["101500", "110000"], "4": ["110500", "115000"],
+          "5": ["115000", "123500"], "6": ["133000", "141500"], "7": ["142000", "150500"], "8": ["151500", "160000"],
+          "9": ["160500", "165000"], "10": ["183000", "191500"], "11": ["192000", "200500"], "12": ["201000", "205500"]}
 patt1 = "[0-9]+"
 patt2 = r"\n(.*)\n"
+patt3 = r"\n(.*)"
 
 
 def read_pdf(full_path):
@@ -25,8 +24,8 @@ def read_pdf(full_path):
         for page in pdf.pages:
             table = page.extract_table()
             for line in table:
+                print(line)
                 course = []
-                # print(line)
                 for elem in line:
                     if elem is not None:
                         tmp = elem.split(" ")
@@ -42,11 +41,10 @@ def read_pdf(full_path):
                         course.append(elem)
                 schedule.append(course)
         return schedule
-
     return None
 
 
-def json_format(full_path):
+def json_format(full_path, start_time):
     calender = {}
     day_sche = {}
     course = {"Course": "", "StartTime": "", "EndTime": "", "StartDate": "",
@@ -54,10 +52,14 @@ def json_format(full_path):
     week = ""
     time = ""
     last = ""
+    standby = ""
+    get_course_name = False
     info = read_pdf(full_path)
     practical_courses = []
     other_courses = []
+
     for i in range(len(info) - 1):
+        print(info[i])
         if i == 0:
             semester = "{}-{}-{}".format(
                 re.findall(patt1, info[i][0])[0],
@@ -65,20 +67,23 @@ def json_format(full_path):
                 re.findall(patt1, info[i][0])[2]
             )
             calender["Semester"] = semester
-            print("calender:", end="\t")
-            print(calender)
-            # print(pdf_info[i])
         else:
             if info[i][0]:
                 if re.match("实践课程：", info[i][0]):
                     for item in info[i]:
                         if item:
-                            practical_courses.append(item)
+                            if re.match("实践课程：", item):
+                                practical_courses.append(re.findall(r"实践课程：(.*)", item)[0])
+                            else:
+                                practical_courses.append(item)
                     continue
                 elif re.match("其它课程：", info[i][0]):
                     for item in info[i]:
                         if item:
-                            other_courses.append(item)
+                            if re.match("其它课程：", item):
+                                other_courses.append(re.findall(r"其它课程：(.*)", item)[0])
+                            else:
+                                other_courses.append(item)
                     continue
 
             for j in range(len(info[i])):
@@ -96,18 +101,44 @@ def json_format(full_path):
                 if not info[i][j]:
                     continue
 
+                # print(info[i][j])
                 get = re.search(patt2, info[i][j])
                 if get:
+                    print("get: ")
+                    print(get)
+                    get_course_name = True
                     course["Course"] = get.group(1)
+                elif standby == "" and re.search(patt3, info[i][j]):
+                    standby = re.search(patt3, info[i][j]).group(1)
 
-            print(last + "\t" + week)
+            course["Location"] = info[i][3]
+            course["Description"] = info[i][4]
+
             if last != "" and last != week:
-                print("======== set =========")
                 calender[last] = copy.deepcopy(day_sche)
-                # print(calender)
                 day_sche.clear()
 
+            if not get_course_name:
+                course["Course"] = standby
+                standby = ""
+            else:
+                get_course_name = False
+
+            week_during = re.findall(patt1, info[i][2])
+            course["StartDate"] = cal_date(start_time, int(week_during[0]), week)
+            course["EndDate"] = cal_date(start_time, int(week_during[1]), week)
+
+            s_section, e_section = re.findall(r"[0-9]+", time)
+            print(s_section, e_section)
+            if re.match("3", course["Location"]) or re.match("2-S", course["Location"]):
+                course["StartTime"] = Time_S[s_section][0]
+                course["EndTime"] = Time_S[e_section][1]
+            else:
+                course["StartTime"] = Time_N[s_section][0]
+                course["EndTime"] = Time_N[e_section][1]
+
             day_sche[time] = copy.deepcopy(course)
+
             print("=== " + week + " " + time + " ===")
             print("day_sche:", end="\t")
             print(day_sche)
@@ -123,18 +154,21 @@ def json_format(full_path):
     return calender
 
 
-def cal_date(start, during):
+def cal_date(start, during, week):
     first = datetime.datetime.strptime(start, "%Y%m%d")
-    during_time = datetime.timedelta(days=7 * (during - 1))
+    during_time = datetime.timedelta(days=7 * (during - 1) + Offset[week])
     first += during_time
     return first.strftime("%Y%m%d")
 
 
 if __name__ == '__main__':
-    path = "./data/"
-    filename = "2020-2021-1.pdf"
-    data = json_format(path + filename)
-    with open("try_write.txt", "w", encoding='utf-8') as f:
+    path = "./data/personal_data/"
+    save_path = "./generated_file/"
+    filename = "2020-2021-2.pdf"
+    start_date = "20210301"
+    data = json_format(path + filename, start_date)
+    generate_file = "{}{}.json".format(save_path, data["Semester"])
+    with open(generate_file, "w", encoding='utf-8') as f:
         json.dump(data, f, ensure_ascii=False)
         print("run")
 
