2a0c40412b872fda04ae6538790b6e3ba29b25be
FreanJa <freanja2730@outlook.com>
2022-02-17 23:06:51 +0800

    Avoid TRAVEL ADVISORY

--- generate_ics_file.py
+++ generate_ics_file.py
@@ -3,7 +3,7 @@ import functions
 
 def generate():
     path = input("JSON file path:\n")
-    ics_path = input("Path to save ics folder:\n")
+    ics_path = input("Path to save ics folder:(Optional, Default: ./generated_file/ics/{json file name}/)\n")
     if ics_path:
         functions.json_to_ics(path, ics_path)
     else:
--- generate_json_file.py
+++ generate_json_file.py
@@ -4,7 +4,7 @@ import functions
 def generate():
     path = input("PDF file path:\n")
     start_date = input("Date of Commencement of Courses:\n")
-    alarm = input("Alarm:\n").split(',')
+    alarm = input("Alarm:(Optional, Default:[-15M, -30M])\n").split(',')
     functions.json_format(path, start_date, alarm)
 
 
